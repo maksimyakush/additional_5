140567ffb1f14d1b10354e2959a6d00231b63e76
maksimyakush <maksimo-1988@yandex.ru>
2017-09-28 23:52:48 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
 	bracketsConfig.forEach((a, i) => {
-		str = str.slice(0, str.indexOf(bracketsConfig[i][0] + bracketsConfig[i][1]))
-		+ str.slice(str.indexOf(bracketsConfig[i][0] + bracketsConfig[i][1]) + 2);			
+		while (str.includes(bracketsConfig[i][0] + bracketsConfig[i][1])) {
+			str = str.slice(0, str.indexOf(bracketsConfig[i][0] + bracketsConfig[i][1]))
+			+ str.slice(str.indexOf(bracketsConfig[i][0] + bracketsConfig[i][1]) + 2);	
+		}		
 	})
  	if (str.length > 0) {
  		return false;
